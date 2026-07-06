718e01ec945dca12b2fca983ad2bfe9ecb8de042
Jochen Wezel <jwezel@compumaster.de>
2026-07-06 20:58:25 +0200

    Prime Linux text shaping before CefGlue startup

--- GuacamoleClient-Avalonia/src/App.axaml.cs
+++ GuacamoleClient-Avalonia/src/App.axaml.cs
@@ -1,6 +1,9 @@
-﻿using Avalonia;
+using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.Media.TextFormatting;
+using System.Globalization;
 
 namespace GuacClient;
 public partial class App : Application
@@ -9,10 +12,21 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        WorkAroundLinuxCefGlueTextShapingCrash();
+
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow();
         }
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private static void WorkAroundLinuxCefGlueTextShapingCrash()
+    {
+        if (!OperatingSystem.IsLinux())
+            return;
+
+        var options = new TextShaperOptions(Typeface.Default.GlyphTypeface, 12, 0, CultureInfo.CurrentCulture, 100);
+        TextShaper.Current.ShapeText("GuacamoleClient".AsMemory(), options);
     }
 }
