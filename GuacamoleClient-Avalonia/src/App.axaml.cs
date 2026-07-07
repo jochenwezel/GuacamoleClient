448603c40eafa26703fdef877961d277f66b4eae
Jochen Wezel <jwezel@compumaster.de>
2026-07-07 12:04:21 +0200

    Set Avalonia Linux window icon

--- GuacamoleClient-Avalonia/src/App.axaml.cs
+++ GuacamoleClient-Avalonia/src/App.axaml.cs
@@ -1,9 +1,12 @@
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
 using Avalonia.Media.TextFormatting;
+using System;
 using System.Globalization;
+using System.IO;
 
 namespace GuacClient;
 public partial class App : Application
@@ -16,9 +19,18 @@ public partial class App : Application
 
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow();
+            desktop.MainWindow = new MainWindow
+            {
+                Icon = LoadApplicationIcon()
+            };
         }
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private static WindowIcon? LoadApplicationIcon()
+    {
+        string iconPath = Path.Combine(AppContext.BaseDirectory, "guac.ico");
+        return File.Exists(iconPath) ? new WindowIcon(iconPath) : null;
     }
 
     private static void WorkAroundLinuxCefGlueTextShapingCrash()
